0a96208c65241ad0e68a994a2c16c9238971e7bf
Blake Summers <summers.blake@gmail.com>
2014-10-30 15:23:14 -0700

    instagram credentials

--- jobs/instadash.rb
+++ jobs/instadash.rb
@@ -2,12 +2,12 @@ require 'instagram'
  
 # Instagram Client ID from http://instagram.com/developer
 Instagram.configure do |config|
-  config.client_id = ''
+  config.client_id = '7242265456564acfb52d53590f3b5c80'
 end
  
 # Latitude, Longitude for location
-instadash_location_lat = '45.429522'
-instadash_location_long = '-75.689613'
+instadash_location_lat = '37.7819203157895'
+instadash_location_long = '-122.404401526284'
  
 SCHEDULER.every '10m', :first_in => 0 do |job|
   photos = Instagram.media_search(instadash_location_lat,instadash_location_long)
